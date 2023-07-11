fdabc631a2db756268b5e83b45f30f896162ff22
Zaid Tahir <zaidtahir524@Gmail.com>
2023-07-11 12:18:48 +0500

    complete famous-quote-2

---
+++ famous-quote-2/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let person_name = "Malcom X";
+let quote = "The future belongs to those who prepare for it today.";
+let composed_message = `${person_name} Once said "${quote}"`;
+console.log(composed_message);
---
+++ famous-quote-2/index.ts
@@ -0,0 +1,4 @@
+let person_name:string = "Malcom X"
+let quote: string = "The future belongs to those who prepare for it today."
+let composed_message: string = `${person_name} Once said "${quote}"`
+console.log(composed_message)
